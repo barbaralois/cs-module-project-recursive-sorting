7ecf97894a2c52bcb1079361d4fec740214cd123
Barbara Moore <barbaraloismoore@gmail.com>
2020-08-19 03:03:25 -0400

    finishing some comments

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,15 +1,25 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
+    # start is longer than the array? item wasn't found
     if start > end:
         return -1
+    # if we're not at the end yet...    
     else:
+        # find the middle value
         middle = start + end // 2
+        # that number is the guess
         guess = arr[middle]
+        # compare to target, if they match
         if guess == target:
+            # return middle (aka the index of that num)
             return middle
+        # compare to target, if target is larger...
         elif target > guess:
+            # recursively call function on items above the mid
             return binary_search(arr, target, (middle+1), end)
+        # compare to target, if target is smaller...
         else:
+            # recursively call function on items below the mid
             return binary_search(arr, target, start, (middle-1))
 
 
